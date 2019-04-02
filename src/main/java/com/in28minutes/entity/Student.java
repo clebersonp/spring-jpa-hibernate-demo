dec95614c0f98ba1d8a3bf4940980ffd1af0cfde
Cleberson Pauluci <pauluci.cleberson@gmail.com>
2019-04-01 23:42:09 -0300

    Adicionado relacionamento entre student e passport e alterada a query inicial

--- src/main/java/com/in28minutes/entity/Student.java
+++ src/main/java/com/in28minutes/entity/Student.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import lombok.Getter;
@@ -26,4 +27,7 @@ public class Student implements Serializable {
 	private Long id;
 
 	private String name;
+
+	@OneToOne // a entidade student é a owner no relacionamento com passport
+	private Passport passport;
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,13 +3,14 @@ insert into course(name, created_date, updated_date) values ('Java', sysdate(), 
 insert into course(name, created_date, updated_date) values ('Dotnet', sysdate(), sysdate());
 insert into course(name, created_date, updated_date) values ('Mongdb', sysdate(), sysdate());
 
-insert into student(name) values ('Fred');
-insert into student(name) values ('Allan');
-insert into student(name) values ('Jane');
-
 insert into passport(number) values ('AB4523');
 insert into passport(number) values ('TRR4545');
 insert into passport(number) values ('ESQ755');
+
+insert into student(name, passport_id) values ('Fred', 1);
+insert into student(name, passport_id) values ('Allan', 2);
+insert into student(name, passport_id) values ('Jane', 3);
+
 
 insert into review(rating, description) values ('5', 'Great Course');
 insert into review(rating, description) values ('3', 'Good Course');
