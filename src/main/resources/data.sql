d3458c0b5ad6bef2c618f0919076475e8108786e
Cleberson Pauluci <pauluci.cleberson@gmail.com>
2019-04-04 23:38:33 -0300

    Criados registros na tabela de relacionamento course_student

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,12 +6,19 @@ insert into course(name, created_date, updated_date) values ('Mongdb', sysdate()
 insert into passport(number) values ('AB4523');
 insert into passport(number) values ('TRR4545');
 insert into passport(number) values ('ESQ755');
+insert into passport(number) values ('WWW899');
 
 insert into student(name, passport_id) values ('Fred', 1);
 insert into student(name, passport_id) values ('Allan', 2);
 insert into student(name, passport_id) values ('Jane', 3);
+insert into student(name, passport_id) values ('Thiago', 4);
 
 
 insert into review(rating, description, course_id) values ('5', 'Great Course', 1);
 insert into review(rating, description, course_id) values ('3', 'Good Course', 1);
 insert into review(rating, description, course_id) values ('5', 'Awesome Course', 2);
+
+insert into course_student(course_id, student_id) values (1, 1);
+insert into course_student(course_id, student_id) values (1, 2);
+insert into course_student(course_id, student_id) values (1, 3);
+insert into course_student(course_id, student_id) values (2, 4);
