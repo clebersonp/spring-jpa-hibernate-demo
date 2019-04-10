7a7d182a6da19339e8e1b0e175e8f4857b7e2e16
Cleberson Pauluci <pauluci.cleberson@gmail.com>
2019-04-09 23:16:37 -0300

    Utilizada a strategy de table_per_class - tabela para cadas classe concreta na heranca

--- src/main/java/com/in28minutes/entity/Employee.java
+++ src/main/java/com/in28minutes/entity/Employee.java
@@ -3,14 +3,12 @@ package com.in28minutes.entity;
 import java.io.Serializable;
 
 import javax.persistence.Column;
-import javax.persistence.DiscriminatorColumn;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
-import javax.persistence.Table;
 
 import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
@@ -21,9 +19,10 @@ import lombok.Setter;
 
 @Entity
 // InheritanceType.SINGLE_TABLE) todos os filhos de employee serão persistidos na mesma tabela employee
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
-@DiscriminatorColumn(name = "employee_type") // para especificar o nome da coluna da herança
-@Table(name = "employee")
+//@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS) // será criada uma tabela para cadas classe concreta
+//@DiscriminatorColumn(name = "employee_type") // para especificar o nome da coluna da herança com a strategy single_table
+//@Table(name = "employee")
 @Getter
 @Setter
 @NoArgsConstructor
@@ -32,7 +31,7 @@ public abstract class Employee implements Serializable {
 	private static final long serialVersionUID = -8795832437936109404L;
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
 	private Long id;
 
 	@Column(nullable = false)
